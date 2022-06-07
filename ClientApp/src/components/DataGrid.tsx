127eb706676ff838ed0a7ea09d9cfaddf2abb3be
Left-Behind-Sic <95462486+Left-Behind-Sic@users.noreply.github.com>
2022-06-07 11:50:49 +0500

    Рабочая таблица с домами

--- ClientApp/src/components/DataGrid.tsx
+++ ClientApp/src/components/DataGrid.tsx
@@ -3,6 +3,7 @@ import { DataGrid } from "@mui/x-data-grid";
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { useEffect } from "react";
 import { fetchHouses } from "../store/reducers/ActionCreators";
+import LinearProgress from "@mui/material/LinearProgress";
 
 export default function BasicColumnsGrid() {
   const { housesPlants, isLoading, error } = useAppSelector(
@@ -11,7 +12,7 @@ export default function BasicColumnsGrid() {
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(fetchHouses());
-  }, []);
+  }, [dispatch]);
 
   let rows = housesPlants.houses.flatMap((house, indexFirst) =>
     house.consumptions.flatMap((item, index) => ({
@@ -28,30 +29,54 @@ export default function BasicColumnsGrid() {
     <div style={{ height: window.innerHeight, width: "100%" }}>
       {isLoading && <h1>Загрузка</h1>}
       {error && <h1>Ошибка</h1>}
-      {console.log(rows)}
+      {console.log(isLoading)}
       <DataGrid
+        // components={{ LoadingOverlay: LinearProgress }}
+        // loading
+        // {...housesPlants}
         experimentalFeatures={{ newEditingApi: true }}
         columns={[
           // { field: "id" },
-          { field: "ConsumerId", headerName: "ID" },
+          { field: "ConsumerId", type: "number", headerName: "ID", flex: 0.2 },
           {
             field: "Name",
             headerName: "Название объекта",
             width: 200,
             editable: true,
+            flex: 1,
+            align: "center",
+            headerAlign: "center",
           },
-          { field: "Date", headerName: "Дата", width: 200, editable: true },
+          {
+            field: "Date",
+            type: "date",
+            valueGetter: ({ value }) => value && new Date(value),
+            headerName: "Дата",
+            width: 200,
+            editable: true,
+            flex: 0.5,
+            align: "center",
+            headerAlign: "center",
+          },
           {
             field: "Weather",
+            type: "number",
             headerName: "Температура",
             width: 160,
             editable: true,
+            flex: 0.5,
+            align: "center",
+            headerAlign: "center",
           },
           {
             field: "Consumption",
+            type: "number",
             headerName: "Потребление",
             width: 160,
             editable: true,
+            flex: 1,
+            align: "center",
+            headerAlign: "center",
           },
         ]}
         rows={rows}
